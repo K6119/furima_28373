03626bda8d991c737514de388daf633510a34346
K6119 <k.adsqu2@gmail.com>
2020-08-15 21:30:55 +0900

    usersモデル作成

---
+++ db/migrate/20200814133233_create_items.rb
@@ -0,0 +1,19 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+
+      t.timestamps
+      t.string :name,             null: false
+      t.binary :image,            null: false
+      t.text :item_description,   null: false
+      t.integer :item_condition,  null: false
+      t.integer :category,        null: false
+      t.integer :delivery_fee,    null: false
+      t.integer :price,           null: false
+      t.integer :shipping_area,   null: false
+      t.integer :shipping_days,   null: false
+      t.integer :user_id,         foreign_key: true
+
+    end
+  end
+end
