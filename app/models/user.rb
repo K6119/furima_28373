087977e5250e48210ab2557292c40f0d5b8ffe99
K6119 <k.adsqu2@gmail.com>
2020-08-17 15:05:47 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+validates :nickname, presence: true
+validates :first_name,format: { /\A[ぁ-んァ-ン一-龥]/ }
+validates :family_name,format: { /\A[ぁ-んァ-ン一-龥]/ }
+validates :first_kana, format: { /\A[ァ-ヶー－]+\z/ }
+validates :family_kana, format: { /\A[ァ-ヶー－]+\z/ }
+validates :email, presence: true
+validates :email, presence: true, uniqueness: true
+validates :email, inclusion: { in: ["@"] }
+validates :password, presence: true
+validates :password, length: { minimum: 6 }
+validates :password, format: { with: /\A[a-zA-Z]+\z/ }
+validates :password, confirmation: true
 end
