3d93c88e73539af95127a9922438d88cc6dc02ed
K6119 <k.adsqu2@gmail.com>
2020-08-27 12:36:29 +0900

    モデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
 end
+
+with_options presence: true do
+  validates :name
+  validates :image
+  validates :item_description
+  validates :item_condition
+  validates :category
+  validates :delivery_fee
+  validates :price
+  validates :shipping_area
+  validates :shipping_days
+end
+
+
+
+has_one :purchase
+belongs_to :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_133233) do
+ActiveRecord::Schema.define(version: 2020_08_25_024416) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id", null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
