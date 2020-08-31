953cdebc3ab4a174e041790a69f0ce6be4d00b40
K6119 <k.adsqu2@gmail.com>
2020-08-31 12:00:17 +0900

    諸々修正、jQery導入

--- db/migrate/20200814133233_create_items.rb
+++ db/migrate/20200814133233_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.timestamps
       t.string :name,             null: false
       t.binary :image,            null: false
-      t.text :item_description_id,   null: false
+      t.text :item_description,   null: false
       t.integer :item_condition_id,  null: false
       t.integer :category_id,        null: false
       t.integer :delivery_fee_id,    null: false
